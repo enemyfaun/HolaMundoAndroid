6f41b1b8cede3b1a038cca33c0b4b5c39fa99ef7
enemyfaun <moramiki@hotmail.com>
2021-11-05 09:13:34 -0600

    Clase Calculadora

---
+++ app/src/main/java/com/gerardo/zamora/jimenez/Calculadora.java
@@ -0,0 +1,39 @@
+package com.gerardo.zamora.jimenez;
+
+public class Calculadora {
+    private double numero1, numero2;
+
+    public double getNumero1() {
+        return numero1;
+    }
+
+    public void setNumero1(double numero1) {
+        this.numero1 = numero1;
+    }
+
+    public double getNumero2() {
+        return numero2;
+    }
+
+    public void setNumero2(double numero2) {
+        this.numero2 = numero2;
+    }
+
+    public double Sumar(){
+        return numero1 + numero2;
+    }
+
+    public double Restar(){
+        return numero1 - numero2;
+    }
+
+    public double Multiplicar(){
+        return numero1 * numero2;
+    }
+
+    public double Dividir(){
+        return numero1 / numero2;
+    }
+
+
+}
