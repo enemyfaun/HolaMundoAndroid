07b6a9c9ec1631c592f33ca1a5549d439242ba52
enemyfaun <moramiki@hotmail.com>
2021-11-08 11:52:10 -0600

    Se agregó método, actividad para la suma

--- app/src/main/java/com/gerardo/zamora/jimenez/MainActivity.java
+++ app/src/main/java/com/gerardo/zamora/jimenez/MainActivity.java
@@ -2,13 +2,30 @@ package com.gerardo.zamora.jimenez;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button buttonSuma;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonSuma = findViewById(R.id.buttonSuma);
+        buttonSuma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirSuma();
+            }
+        });
+    }
+
+    private void abrirSuma(){
+        Intent Intent = new Intent(this, SumaActivity.class);
+        startActivity(Intent);
     }
 }
---
+++ app/src/main/java/com/gerardo/zamora/jimenez/RestaActivity.java
@@ -0,0 +1,15 @@
+
+package com.gerardo.zamora.jimenez;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RestaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resta);
+    }
+}
